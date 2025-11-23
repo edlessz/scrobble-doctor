a802f5f25011ec14bfe577ce8c4a14cccc4140b2
edlessz <42257904+edlessz@users.noreply.github.com>
2025-11-22 23:01:22 -0500

    chore: add deployment scripts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import pkg from "./package.json";
 
-// https://vite.dev/config/
 export default defineConfig({
+	base: `/${pkg.name}/`,
 	plugins: [react(), tailwindcss()],
 });
